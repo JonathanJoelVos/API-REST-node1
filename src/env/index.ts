1422df700df97452926fdc58a2c3bba26c152098
JonathanJoelVos <dev.jonathan.joel.vos@gmail.com>
2023-07-31 20:00:58 -0300

    Configuração do banco

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,10 +1,19 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
+
+if (process.env.NODE_ENV === 'test') {
+  config({
+    path: '.env.test',
+  })
+} else {
+  config()
+}
 
 const envSchema = z.object({
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['pg', 'sqlite3']),
 })
 
 const _env = envSchema.safeParse(process.env)
